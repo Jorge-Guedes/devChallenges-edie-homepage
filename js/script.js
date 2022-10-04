e30301d485b73ca901231d520e593d0069d90add
Jorge Guedes <jorge_guedes00@hotmail.com>
2022-10-04 13:30:37 -0300

    Add js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const btnMobile = document.getElementById('button-mobile');
+const linkMobile = document.querySelectorAll('#nav-menu a');
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.toggle('active');
+    const active = navMenu.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+      event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+      event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+linkMobile.forEach(link => {
+  link.addEventListener('click', toggleMenu);
+});
